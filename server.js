989ac013e0d880455cffce2902d1a27e34f363db
Bennett <jbennett1@ebay.com>
2018-06-25 16:56:12 -0700

    added in views and partials

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require("express");
+const hbs = require("hbs");
 
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
 app.get('/', (req, res) => {
   // res.send("<h1>Hello from Express!</h1>");
-  res.send({
-    name: "James",
-    likes: ['Compupu', 'Food']
-  });
+  res.render("home.hbs", {
+    pageTitle: 'Home',
+    welcomeMessage: "Welcome to my homepage."
+  })
 });
 
 app.get('/about', (req, res) => {
-  res.send("<h1>About page</h1>");
+  res.render("about.hbs", {
+    pageTitle: 'About page',
+  });
 });
 
 app.get('/bad', (req, res) => {
@@ -20,4 +31,4 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(3002);
